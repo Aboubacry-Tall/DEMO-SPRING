6982fb8a14d6b8944e70be6407ce00dbe9dc103b
Ghost Tall <diarnotall@gmail.com>
2021-06-23 14:17:42 -0700

    paiements

---
+++ Maurilib/src/main/java/com/libsys/Maurilib/controller/BuyController.java
@@ -0,0 +1,34 @@
+package com.libsys.Maurilib.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.libsys.Maurilib.model.Buy;
+import com.libsys.Maurilib.repository.BuyReposetory;
+
+@RestController
+@RequestMapping("/lib/")
+@CrossOrigin(origins = "http://localhost:4200")
+public class BuyController {
+	@Autowired
+	private BuyReposetory buyRepo;
+	
+	@PostMapping("/buy")
+	public void buy(@RequestBody Buy buy) {
+		System.out.println("Post" + buy.getLivreId());
+		buyRepo.save(buy);
+	}
+	
+	@GetMapping("/buys")
+	public List<Buy> buys() {
+		System.out.println("Get");
+		return buyRepo.findAll();
+	}
+}
---
+++ Maurilib/src/main/java/com/libsys/Maurilib/repository/BuyReposetory.java
@@ -0,0 +1,9 @@
+package com.libsys.Maurilib.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.libsys.Maurilib.model.Buy;
+
+public interface BuyReposetory extends JpaRepository<Buy,Long>{
+	
+}
